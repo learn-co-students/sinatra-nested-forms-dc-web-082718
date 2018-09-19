35d078d4cfa88d8e00fd9d90d3e204d79c36cc4e
drubaby <peryton6@gmail.com>
2018-09-19 15:48:32 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
-class Pirate
+class Pirate 
+
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(name=nil, weight=nil, height=nil)
+    @name, @weight, @height = name, weight, height
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+
+  @@all = []
+
+  attr_accessor :name, :type, :booty
+
+  def initialize(name = nil, type= nil, booty = nil)
+     @name, @type, @booty = name, type, booty
+     @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
 end
